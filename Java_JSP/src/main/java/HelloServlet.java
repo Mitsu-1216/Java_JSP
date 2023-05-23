5e3fad41a3486c20f8c31eb4ab3667933f3c95ab
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-05-24 07:11:00 +0900

    3_2e

--- Java_JSP/src/main/java/HelloServlet.java
+++ Java_JSP/src/main/java/HelloServlet.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +13,11 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request
 			,HttpServletResponse response)
 		throws ServletException,IOException{
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		out.printlen("<html>");
+		out.printlen("・・・");
+		out.printlen("</html>");
 	}
 
 }
