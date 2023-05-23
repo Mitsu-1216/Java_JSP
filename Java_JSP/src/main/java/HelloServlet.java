cce33a0e8db6524b92b7d85a0be1983307cbb07b
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-05-24 07:02:51 +0900

    3_1

--- Java_JSP/src/main/java/HelloServlet.java
+++ Java_JSP/src/main/java/HelloServlet.java
@@ -6,7 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// もととなるクラスを継承
 public class HelloServlet extends HttpServlet {
+	// doGet(メインのメソッド)をオーバーライド
 	protected void doGet(HttpServletRequest request
 			,HttpServletResponse response)
 		throws ServletException,IOException{
