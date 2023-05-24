aa32ea0c62d64ea0fd161aab43b6785dbeaa27a3
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-05-24 22:05:15 +0900

    3_4

---
+++ Java_JSP/src/webapp/SampleServlet.java
@@ -0,0 +1,16 @@
+import java.io.IOExseption;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+
+
+@WebServlet("/SampleServlet")
+public class SampleServlet extends HttpServlet{
+	private static final long seriaVersionUID 	= 1L;
+	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+	throws ServletException,IOException{
+		String[] animalArray= {"うさぎ","パンダ","ライオン"};
+	}
+		
+}
