17178fcd6b4caafa02597a33f8e212e824b493a3
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-05-24 22:11:38 +0900

    3_4

--- Java_JSP/src/webapp/SampleServlet.java
+++ Java_JSP/src/webapp/SampleServlet.java
@@ -2,6 +2,8 @@ import java.io.IOExseption;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 
+import javax.servlet.http.HttpServletResponse;
+
 
 @WebServlet("/SampleServlet")
 public class SampleServlet extends HttpServlet{
@@ -11,6 +13,26 @@ public class SampleServlet extends HttpServlet{
 			HttpServletResponse response)
 	throws ServletException,IOException{
 		String[] animalArray= {"うさぎ","パンダ","ライオン"};
+		int index =(int)(Math.random()*3);
+		String animal=animalArray[index];
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM月日dd");
+		String today_date = sdf.format(date);
+		
+		// HTMLを出力
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>動物選び</title>");
+		out.println("</head>");
+		out.println("<p>"+today+"のアニマルは"+luck+"です！"</p>);
+		out.println("</body>");
+		out.println("</html>");
+	
+	
 	}
+	
 		
 }
