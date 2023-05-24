ab2c7c5d767ff7d9af76d499d8f2a0535813d6e6
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-05-25 06:35:29 +0900

    Servlet

---
+++ Java_JSP/src/main/java/servlet/SampleServlet.java
@@ -0,0 +1,45 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SampleServlet
+ */
+@WebServlet("/SampleServlet")
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] animalArray= {"うさぎ","パンダ","ライオン"};
+		int index =(int)(Math.random()*3);
+		String animal = animalArray[index];
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM月dd日");
+		String today_date = sdf.format(date);
+		
+		// HTMLを出力
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>動物選び</title>");
+		out.println("</head>");
+		out.println("<p>"+ today_date +"のアニマルは" + animal + "です！</p>");
+		out.println("</body>");
+		out.println("</html>");
+       
+		}
+	
+	}
+
