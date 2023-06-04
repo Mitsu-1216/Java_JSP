30d15318d1b6eee1478aa7ae0c683b9fdf5e4189
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-06-04 09:56:16 +0900

    JavaBeans

---
+++ Java_JSP/src/webapp/model/Animal.java
@@ -0,0 +1,22 @@
+package model;
+import java.io.Serializable;
+
+public clask Human implements Serializable {
+	private String name;
+	private int age;
+	
+}
+
+public Aonimal () {
+	public Human(String name,int age) {
+	this.name=name;
+	this.age=age;
+		
+	}
+	
+	public String getName() {return name;}
+	public void setName(String name) {this.name=name;}
+	public int getAge() {return age;}
+	public void setAge(int age) {this.age=age;}
+	
+}
