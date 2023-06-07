0a8ff72892a8cf0a5ef595340f1c7e5a0965ef70
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-06-07 21:30:09 +0900

    modify

--- Java_JSP/src/main/java/Redirect6_3.java
+++ Java_JSP/src/main/java/Redirect6_3.java
@@ -15,17 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 public class Redirect6_3 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Redirect6_3() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("/Java_JSP/SampleServlet");
 		
