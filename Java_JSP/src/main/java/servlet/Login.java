7321d78167f1b881e8364b70f962c8871359663f
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-06-13 22:30:08 +0900

    servlet

--- Java_JSP/src/main/java/servlet/Login.java
+++ Java_JSP/src/main/java/servlet/Login.java
@@ -6,16 +6,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.RequestDispatcher;
+import model.LoginLogic;
+import model.User;
 
-/**
- * Servlet implementation class Login
- */
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -28,9 +28,15 @@ public class Login extends HttpServlet {
 		User user = new User(name,pass);
 		
 		LoginLogic loginLogic= new LoginLogic();
-		boolena isLogin = LoginLogic.excute(user);
+		boolean isLogin = loginLogic.excute(user);
 		
+		if(isLogin) {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser",user);
+		}
 		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/loginResult.jsp");
+		dispatcher.forward(request,response);
 	}
 
 }
